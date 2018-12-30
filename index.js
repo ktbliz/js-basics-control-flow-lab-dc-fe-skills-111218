602d86c7123288171f8a1b2de3e446ed37e644fb
Katie Blizzard <kblizzard11@gmail.com>
2018-12-30 01:58:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400 ) {
+    result = "This one is on me!";
+  }
+    
+  else if (feet > 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+    
+  else if (feet > 2500) {
+    result = "No can do.";
+  }
+     
+  return result;
+}
+
+scuberGreetingForFeet(191)
+  
+
+function ternaryCheckCity(city) {
+  const city;
+  city === "NYC" ? "Ok, sounds good" : "No go.";
+}
+
+
+
+switchOnCharmFromTip()
