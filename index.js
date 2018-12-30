f29ada2ccd569e14cec5c134133a7a8b9dc0782e
Katie Blizzard <kblizzard11@gmail.com>
2018-12-30 02:18:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 function scuberGreetingForFeet(feet) {
   if (feet <= 400 ) {
-    result = "This one is on me!";
+    let result = "This one is on me!";
   }
     
   else if (feet > 2000 && feet <= 2500) {
-    result = "I will gladly take your thirty bucks.";
+    let result = "I will gladly take your thirty bucks.";
   }
     
   else if (feet > 2500) {
-    result = "No can do.";
+    let result = "No can do.";
   }
      
   return result;
